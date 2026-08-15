fac718d3c269c220cf41f245df6befb27a53ec11
Link4Electronics <link4electronics@gmail.com>
2026-08-15 06:49:01 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,7 +3,7 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q supermodel-git | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q supermodel-git | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook:wayland-is-broken.hook"
@@ -22,4 +22,4 @@ quick-sharun --make-appimage
 
 # Test the app for 12 seconds, if the app normally quits before that time
 # then skip this or check if some flag can be passed that makes it stay open
-#quick-sharun --test ./dist/*.AppImage
+quick-sharun --simple-test ./dist/*.AppImage
