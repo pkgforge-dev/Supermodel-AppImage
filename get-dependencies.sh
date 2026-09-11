7ece1868f46359be634dfb0337b9263a071f2411
Link4Electronics <link4electronics@gmail.com>
2026-09-10 22:45:24 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,9 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-    glu      \
-    sdl2_net
+pacman -Syu --noconfirm glu sdl2_net
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
